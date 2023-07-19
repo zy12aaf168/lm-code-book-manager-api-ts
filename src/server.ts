7cb7687d5a71c42c416613e9280a59455bc2f616
Zulfu Yilmaz <z.yilmaz08@gmail.com>
2023-07-19 21:49:28 +0100

    Added new config file to swap in postgres database in production mode

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import * as dotenv from "dotenv";
+const environment = process.env.NODE_ENV || "dev";
+dotenv.config({ path: `.env.${environment}` });
+
 import { app } from "./app";
 import { populateDummyData } from "./database/database_seed";
 
-const environment = process.env.NODE_ENV || "dev";
 const PORT = 3000;
 
 console.log(`🌍 Running in ${environment} environment`);
