af5edb85096699580e38b2e6f93db8221a1622e8
Zulfu Yilmaz <z.yilmaz08@gmail.com>
2023-07-19 21:50:37 +0100

    Added new configuration so that postgres implementation is swapped in when in production mode

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,13 @@
-import { Sequelize } from "sequelize";
-
-// TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+import { Dialect, Sequelize } from "sequelize";
+export let sequelize = new Sequelize("sqlite::memory:");
+if (process.env.NODE_ENV !== 'test') {
+    sequelize = 
+        new Sequelize(
+            process.env.DB_NAME ?? 'MISSING_DB_NAME_CONFIG',
+            process.env.DB_USERNAME ?? 'MISSING_DB_USERNAME_CONFIG',
+            process.env.DB_PASSWORD ?? 'MISSING_DB_PASSWORD_CONFIG', {
+            host: process.env.DB_HOST ?? 'MISSING_DB_HOST_CONFIG',
+            port: parseInt(process.env.DB_PORT as string) ?? "MISSING_DB_PORT_CONFIG",
+            dialect: (process.env.DB_DIALECT as Dialect) ?? 'postgres',
+    });
+}
